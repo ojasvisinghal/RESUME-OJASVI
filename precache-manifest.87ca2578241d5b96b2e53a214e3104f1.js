f308d95c4cd061f0be8e42319d0f0f1c3de72cc2
ojasvisinghal <osinghal00@gmail.com>
2019-10-01 08:02:44 +0530

    Updates

---
+++ precache-manifest.87ca2578241d5b96b2e53a214e3104f1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc5791e6b2e127b0ab0610d2d0074bf",
+    "url": "/resume-ojasvi/index.html"
+  },
+  {
+    "revision": "abf07d59a7237fa8b445",
+    "url": "/resume-ojasvi/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ebf6a4e4f86b1ceb9f97",
+    "url": "/resume-ojasvi/static/js/2.d935e84b.chunk.js"
+  },
+  {
+    "revision": "abf07d59a7237fa8b445",
+    "url": "/resume-ojasvi/static/js/main.91c028e4.chunk.js"
+  },
+  {
+    "revision": "9d16bf034a5b8aafec57",
+    "url": "/resume-ojasvi/static/js/runtime~main.d3e1de31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume-ojasvi/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a27353310fbd0ffbd165c3f3bc1fd979",
+    "url": "/resume-ojasvi/static/media/ojasvi.a2735331.jpg"
+  },
+  {
+    "revision": "b393ef016ab40ec3cfb81990e0193bcc",
+    "url": "/resume-ojasvi/static/media/ojasvi.b393ef01.png"
+  },
+  {
+    "revision": "363ba016a27a0bb7649deb42b01735a4",
+    "url": "/resume-ojasvi/static/media/photo.363ba016.png"
+  },
+  {
+    "revision": "8756159fd43785e5bf961b4f62d62a4f",
+    "url": "/resume-ojasvi/static/media/poetry.8756159f.png"
+  },
+  {
+    "revision": "6ddd636a151046ad5f84bca3d4cbd877",
+    "url": "/resume-ojasvi/static/media/sketch.6ddd636a.png"
+  }
+]);
